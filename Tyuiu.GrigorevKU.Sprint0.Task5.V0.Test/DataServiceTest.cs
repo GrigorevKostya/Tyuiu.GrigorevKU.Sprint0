87edece174cf92181c84aec024114e6c85edcd86
Tyuiu.GrigorevKU@outlook.com <uhbujhmtdrjcnz@gmail.com>
2023-09-08 21:45:13 +0500

    Task6

--- Tyuiu.GrigorevKU.Sprint0.Task5.V0.Test/DataServiceTest.cs
+++ Tyuiu.GrigorevKU.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -12,17 +12,17 @@ namespace Tyuiu.GrigorevKU.Sprint0.Task5.V0.Test
         {
             Assert.AreEqual(10, DataService.Addition(5, 5));
         }
-
+        [TestMethod]
         public void CheckedSubstractionValid()
         {
             Assert.AreEqual(5, DataService.Substraction(10, 5));
         }
-
+        [TestMethod]
         public void CheckedMultiplicationValid()
         {
             Assert.AreEqual(25, DataService.Multiplication(5, 5));
         }
-
+        [TestMethod]
         public void CheckedDivisionValid()
         {
             Assert.AreEqual(3, DataService.Division(9, 3));
---
+++ Tyuiu.GrigorevKU.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GrigorevKU.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GrigorevKU.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed79dcc4-273a-4698-b4ca-c0fe670814e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
